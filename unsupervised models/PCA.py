35b7f9faeb35794e7f29650d2e121b549e7e5fc5
ehm <alafago05@gmail.com>
2026-01-08 20:46:14 +0100

    Add files via upload

---
+++ unsupervised models/PCA.py
@@ -0,0 +1,113 @@
+'''In this lab you will explore how to implement two important applications of PCA.
+
+The first application illustrates how you can use PCA to project 2-D data onto its principal axes,
+meaning the two orthogonal directions that explain most of the variance in your data.
+For the second application, you will use PCA to project higher dimensional data down to a lower dimensional feature space.
+This is an example of dimension reduction, a powerful technique that has multiple benefits,
+including reducing your model-building computational load and, in many cases, the accuracy of your model. PCA can help you filter out redundant,
+linearly correlated variables and reduce the amount of noise in your data. '''
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+
+np.random.seed(42)
+mean = [0, 0]
+cov = [[3, 2], [2, 2]]
+X = np.random.multivariate_normal(mean=mean, cov=cov, size=200)
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1],  edgecolor='k', alpha=0.7)
+plt.title("Scatter Plot of Bivariate Normal Distribution")
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.axis('equal')
+plt.grid(True)
+plt.show()
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+
+components = pca.components_
+print(components)
+print(pca.explained_variance_ratio_)
+projection_pc1 = np.dot(X, components[0])
+projection_pc2 = np.dot(X, components[1])
+
+x_pc1 = projection_pc1 * components[0][0]
+y_pc1 = projection_pc1 * components[0][1]
+x_pc2 = projection_pc2 * components[1][0]
+y_pc2 = projection_pc2 * components[1][1]
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], label='Original Data', ec='k', s=50, alpha=0.6)
+
+# Plot the projections along PC1 and PC2
+plt.scatter(x_pc1, y_pc1, c='r', ec='k', marker='X', s=70, alpha=0.5, label='Projection onto PC 1')
+plt.scatter(x_pc2, y_pc2, c='b', ec='k', marker='X', s=70, alpha=0.5, label='Projection onto PC 2')
+plt.title('Linearly Correlated Data Projected onto Principal Components', )
+plt.xlabel('Feature 1',)
+plt.ylabel('Feature 2',)
+plt.legend()
+plt.grid(True)
+plt.axis('equal')
+plt.show()
+
+# Load the Iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+target_names = iris.target_names
+
+# Standardize the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+iris.target_names
+# Apply PCA and reduce the dataset to 2 components
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_scaled)
+plt.figure(figsize=(8,6))
+
+colors = ['navy', 'turquoise', 'darkorange']
+lw = 1
+
+for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+    plt.scatter(X_pca[y == i, 0], X_pca[y == i, 1], color=color, s=50, ec='k',alpha=0.7, lw=lw,
+                label=target_name)
+
+plt.title('PCA 2-dimensional reduction of IRIS dataset',)
+plt.xlabel("PC1",)
+plt.ylabel("PC2",)
+plt.legend(loc='best', shadow=False, scatterpoints=1,)
+# plt.grid(True)
+plt.show()
+100*pca.explained_variance_ratio_.sum()
+
+# Standardize the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Apply PCA
+pca = PCA()
+X_pca = pca.fit_transform(X_scaled)
+
+# Explained variance ratio
+explained_variance_ratio = pca.explained_variance_ratio_
+
+# Plot explained variance ratio for each component
+plt.figure(figsize=(10,6))
+plt.bar(x=range(1, len(explained_variance_ratio)+1), height=explained_variance_ratio, alpha=1, align='center', label='PC explained variance ratio' )
+plt.ylabel('Explained Variance Ratio')
+plt.xlabel('Principal Components')
+plt.title('Explained Variance by Principal Components')
+
+# Plot cumulative explained variance
+cumulative_variance = np.cumsum(explained_variance_ratio)
+plt.step(range(1, 5), cumulative_variance, where='mid', linestyle='--', lw=3,color='red', label='Cumulative Explained Variance')
+# Only display integer ticks on the x-axis
+plt.xticks(range(1, 5))
+plt.legend()
+plt.grid(True)
+plt.show()
